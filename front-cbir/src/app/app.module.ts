b9ce142d6878c131f6a90744090509dd20e5ab9d
luisa <luisita0518@unicauca.edu.co>
2020-12-12 22:19:36 -0500

    inicio

--- front-cbir/src/app/app.module.ts
+++ front-cbir/src/app/app.module.ts
@@ -2,14 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
