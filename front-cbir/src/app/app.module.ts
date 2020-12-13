b649fd4cfe07632600e5f306790037927a05f13f
luisa <luisita0518@unicauca.edu.co>
2020-12-12 23:47:23 -0500

    cropper

--- front-cbir/src/app/app.module.ts
+++ front-cbir/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { AngularCropperjsModule} from 'angular-cropperjs';
+//import { RouterModule, Routes } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
+
+/*const routes: Routes = [
+  { path: " ", component: InicioComponent }
+]*/
 
 @NgModule({
+  
   declarations: [
     AppComponent,
     InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //RouterModule.forRoot(routes),
+    HttpClientModule,
+    AngularCropperjsModule
   ],
   
   providers: [],
--- front-cbir/src/app/inicio/inicio.component.ts
+++ front-cbir/src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {CropperComponent} from 'angular-cropperjs';
 
 @Component({
   selector: 'app-inicio',
@@ -6,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
+  @ViewChild('angularCropper') angularCropper: CropperComponent;
+  imgUrl: string;
+  imgResul: string;
   constructor() { }
+  selectArchivo(event){
+    if(event.target.files && event.target.files[0]){
+      const leeArchivo = new FileReader();
+      leeArchivo.readAsDataURL(event.target.files[0]);
+      leeArchivo.onload=()=>{
+        this.imgUrl = leeArchivo.result as string;
+      }
+    }
+  }
+
+  obtenerImg(){
+    this.angularCropper.cropper.getCroppedCanvas().toBlob((blob)=>{
+      const leer = new FileReader();
+      leer.readAsDataURL(blob);
+      leer.onload = () => {
+        this.imgResul = leer.result as string;
+      }
+    },'image/jpeg',0.95); 
+    console.log('path'+this.imgResul);
+  }
 
   ngOnInit(): void {
   }
